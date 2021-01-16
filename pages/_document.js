aadb62b09778c21d1fd2d2f395555f04091c93e4
Rakshit Maini <rakshit.maini@bivocalbirds.com>
2021-01-16 18:46:35 +0530

    updated with css delay fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Children } from "react";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
@@ -33,4 +35,27 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument;
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...Children.toArray(initialProps.styles),
+        sheet.getStyleElement(),
+      ],
+    };
+  } finally {
+    sheet.seal();
+  }
+};
